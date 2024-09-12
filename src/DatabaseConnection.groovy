bf6df564538a55ab72a8f15c031e0a4db27466b9
ubuntu_mateus <henriquemateus2502@gmail.com>
2024-09-12 16:45:19 -0300

    change DB Connection to Sql api

--- src/DatabaseConnection.groovy
+++ src/DatabaseConnection.groovy
@@ -1,13 +1,15 @@
-import java.sql.Connection
-import java.sql.DriverManager
+import groovy.sql.Sql
+import groovy.transform.CompileStatic
 
+@CompileStatic
 class DatabaseConnection {
 
     private static final String URL = "jdbc:postgresql://localhost:5432/postgres"
     private static final String USER = "mateus"
     private static final String PASSWORD = "gringo"
+    private static final String DRIVER = 'org.postgresql.Driver'
 
-    private static Connection getConnection(){
-        return DriverManager.getConnection(URL, USER, PASSWORD);
+    private static Sql getConnection(){
+        return Sql.newInstance(URL,USER, PASSWORD, DRIVER)
     }
 }
