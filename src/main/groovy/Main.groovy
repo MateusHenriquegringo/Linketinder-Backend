aeb8df1a7ffa2975d41f95c58b38b5489b04cd52
ubuntu-mateus <henriquemateus2502@gmail.com>
2024-09-17 18:40:18 -0300

    remove mocks

--- src/main/groovy/Main.groovy
+++ src/main/groovy/Main.groovy
@@ -1,23 +1,7 @@
-import DAO.CandidatoDAO
-import DAO.CompetenciasDAO
-import DAO.EmpresaDAO
-import DAO.VagaDAO
-import enums.CompetenciasENUM
-import model.Candidato
-import model.Competencia
-import model.Empresa
-import model.Vaga
 
 class Main {
 
-    static CompetenciasDAO competenciasDAO = new CompetenciasDAO()
-    static CandidatoDAO dao = new CandidatoDAO();
-    static VagaDAO vagaDAO = new VagaDAO();
-    static EmpresaDAO empresaDAO = new EmpresaDAO();
-
     static void main(String[] args) {
-
-
 
     }
 }
