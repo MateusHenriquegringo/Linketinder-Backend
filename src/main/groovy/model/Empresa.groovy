c59cd11b50b07e911f25f012eea8943ee2b30079
ubuntu-mateus <henriquemateus2502@gmail.com>
2024-10-03 20:44:38 -0300

    remove dtos, remove unused methods

--- src/main/groovy/model/Empresa.groovy
+++ src/main/groovy/model/Empresa.groovy
@@ -1,7 +1,5 @@
 package model
 
-import DTO.Request.EmpresaRequestDTO
-
 class Empresa {
 
     long id
@@ -15,15 +13,5 @@ class Empresa {
     String password
 
     Empresa () {}
-
-    Empresa(EmpresaRequestDTO dto) {
-        this.empresa_name = dto.empresa_name()
-        this.description = dto.description()
-        this.email = dto.email()
-        this.CNPJ = dto.cnpj()
-        this.CEP = dto.cep()
-        this.country = dto.country()
-        this.password = dto.password()
-    }
 
 }
